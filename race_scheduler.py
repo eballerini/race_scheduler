abc93ab9bbd401da05dc9bef3b4d7a04a1f346b2
Emmanuel B <emmanuel.ballerini@gmail.com>
2022-12-24 12:06:01 -0500

    create Input class, add --create-entries arg

--- race_scheduler.py
+++ race_scheduler.py
@@ -13,6 +13,14 @@ class Week:
         return f"week num: {self.week_num}"
 
 
+class Input:
+    def __init__(self, filename, race_date, is_time_based, create_entries):
+        self.filename = filename
+        self.race_date = race_date
+        self.is_time_based = is_time_based
+        self.create_entries = create_entries
+
+
 def read_file(filename):
     weeks = []
     with open(filename) as file_in:
@@ -45,32 +53,49 @@ def format_date(d):
 
 
 def print_usage_and_exit():
-    print("usage: python race_scheduler.py <schedule.csv> <race_date_in_YYYY-MM-DD> <distance|time>")
+    print("usage: python race_scheduler.py <schedule.csv> <race_date_in_YYYY-MM-DD> <distance|time> [--create-entries]")
     sys.exit()
 
 
-def main():
-    print(f"Welcome to the race scheduler!")
-    if len(sys.argv) != 4:
+def validate_args(args):
+    if len(args) < 4:
         print("Not enough arguments")
         print_usage_and_exit()
 
-    filename = sys.argv[1]
-    race_date_raw = sys.argv[2]
-    unit = sys.argv[3]
+    filename = args[1]
+    race_date_raw = args[2]
+    unit = args[3]
+    create_entries = False
+    if len(args) == 5:
+        if args[4] == "--create-entries":
+            create_entries = True
+        else:
+            print("Error: unrecognized argument")
+            print_usage_and_exit()
+    elif len(args) > 5:
+        print("Too many arguments")
+        print_usage_and_exit()
+
     if unit != "distance" and unit != "time":
         print_usage_and_exit()
 
     is_time_based = unit == "time"
-
-    weeks = read_file(filename)
     race_day = datetime.strptime(race_date_raw, "%Y-%m-%d")
-    current_date = race_day
+
+    return Input(filename, race_day, is_time_based, create_entries)
+
+
+def main():
+    print(f"Welcome to the race scheduler!")
+    input_args = validate_args(sys.argv)
+
+    weeks = read_file(input_args.filename)
+    current_date = input_args.race_date
     today = datetime.today().date()
     for w in reversed(weeks):
         for d in reversed(w.days):
             if d.lower() != "rest":
-                if is_time_based:
+                if input_args.is_time_based:
                     w.training_days.insert(0, (d, current_date))
                 else:
                     w.training_days.insert(0, (float(d), current_date))
@@ -94,7 +119,7 @@ def main():
                     here = " ****** IT IS TODAY ******"
                     found_today = True
 
-            if is_time_based:
+            if input_args.is_time_based:
                 print(format_date(day) + ": " + activity + here)
             else:
                 print(format_date(day) + ": " + "{:2.1f}".format(activity) + " km" + here)
